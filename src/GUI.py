d7ab84f32c8405e1214496c7a3f14387cb4e7780
Vojta <vojta.karasek1@gmail.com>
2023-11-22 22:31:52 +0100

    GUI progress

--- src/GUI.py
+++ src/GUI.py
@@ -2,37 +2,45 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 
-main = Tk()
 
-screen_height = main.winfo_screenheight()
-screen_width = main.winfo_screenwidth()
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Workout planner")
 
-frm1 = ttk.Frame(main, padding=10)
-frm1.place(x=screen_width/2, y=screen_height/2)
+        self.screen_height = self.master.winfo_screenheight()
+        self.screen_width = self.master.winfo_screenwidth()
 
-ttk.Button(main, text="Exit", command=main.destroy).grid(column=0, row=0)
+    def frames(self):
+        frm1 = ttk.Frame(self.master, padding=10)
+        frm1.place(x=self.screen_width / 2, y=self.screen_height / 2)
 
-ttk.Label(frm1, text="Vyberte část těla, kterou chcete posilovat").grid(column=1,row=0)
+        ttk.Button(self.master, text="Exit", command=self.master.destroy).grid(column=0, row=0)
 
-current_var = tkinter.StringVar()
-Bp_combobox = ttk.Combobox(frm1, textvariable=current_var)
-Bp_combobox.grid(column=1, row=1)
-Bp_combobox['values'] = ('břicho', 'záda', 'paže', 'prsa', 'ramena', 'stehna', 'lýtka')
-Bp_combobox['state'] = ('readonly')
+        ttk.Label(frm1, text="Vyberte část těla, kterou chcete posilovat").grid(column=1, row=0)
 
-ttk.Label(frm1, text="vyberte obtížnost").grid(column=1, row=2)
+        current_var = tkinter.StringVar()
+        bp_combobox = ttk.Combobox(frm1, textvariable=current_var)
+        bp_combobox.grid(column=1, row=1)
+        bp_combobox['values'] = ('břicho', 'záda', 'paže', 'prsa', 'ramena', 'stehna', 'lýtka')
+        bp_combobox['state'] = ('readonly')
 
-current_var2 = tkinter.StringVar()
-Lvl_combobox = ttk.Combobox(frm1, textvariable=current_var2)
-Lvl_combobox.grid(column=1, row=3)
-Lvl_combobox['values'] = ('začátečník', 'středně pokročilý', 'pokročilý')
-Lvl_combobox['state'] = ('readonly')
+        ttk.Label(frm1, text="vyberte obtížnost").grid(column=1, row=2)
 
-frame_width = frm1.winfo_reqwidth()
-frame_height = frm1.winfo_reqheight()
-
+        current_var2 = tkinter.StringVar()
+        lvl_combobox = ttk.Combobox(frm1, textvariable=current_var2)
+        lvl_combobox.grid(column=1, row=3)
+        lvl_combobox['values'] = ('začátečník', 'středně pokročilý', 'pokročilý')
+        lvl_combobox['state'] = ('readonly')
 
 
-main.attributes('-fullscreen', True)
+def main():
+    root = Tk()
+    gui = GUI(root)
+    gui.frames()
+    root.attributes('-fullscreen', True)
+    root.mainloop()
 
-main.mainloop()
+
+if __name__ == "__main__":
+    main()
