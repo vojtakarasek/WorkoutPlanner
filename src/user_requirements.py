75b4ba1e8362640f75b822450f3f53ea07d77868
Vojta <vojta.karasek1@gmail.com>
2023-12-20 20:11:57 +0100

    starting with the reqs

--- src/user_requirements.py
+++ src/user_requirements.py
@@ -1,3 +1,19 @@
+from starting_screen import Frame1
+from body_part_enum import BodyPart
+
+
 class UserRequirements:
-    def requirements(self):
-        pass
+    def requirements(self, frame1) -> list[BodyPart]:
+        body_parts = []
+        selected_bp = frame1.combobox_variables_bp()
+        for i in selected_bp:
+            if len(i) == 0:
+                pass
+            else:
+                body_parts.append(BodyPart(i))
+
+        return body_parts
+
+
+frame1 = Frame1
+
