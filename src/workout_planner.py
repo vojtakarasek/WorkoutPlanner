65714ccc098c03c6109049a97a33df7eb4b88039
Vojta <vojta.karasek1@gmail.com>
2023-12-28 21:25:44 +0100

    clearing

--- src/workout_planner.py
+++ src/workout_planner.py
@@ -1,5 +1,4 @@
 from exercise_repository import ExerciseRepository
-from workout import Workout
 from user_requirements import UserRequirements
 
 
@@ -7,6 +6,5 @@ class WorkoutPlanner:
     def __init__(self, repository: ExerciseRepository):
         self.repository = repository
 
-    def create_plan(self, user_req: UserRequirements): #-> Workout:  # returns class Workout
-        #body_parts_given = user_req.body_part_reqs()
+    def create_plan(self, user_req: UserRequirements):
         return self.repository.get_for_body_part(user_req)
